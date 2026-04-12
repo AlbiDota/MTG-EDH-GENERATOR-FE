656e863d47860f9951895bb08c4c877ab8f8e70b
AlbiDota <alisaele1@gmail.com>
2026-04-13 01:27:37 +0200

    changed url, the backend is now LIVE

--- src/components/outputs/deckGrid/apiCall.js
+++ src/components/outputs/deckGrid/apiCall.js
@@ -1,9 +1,7 @@
 export async function apiCall(param) {
     // const url = process.env.url || "http://localhost:3001/deckbuilder"
-    const url = "http://localhost:3001/deckbuilder"
-    // const url = "http://mtg-backend-env.eba-ngp9ifxe.eu-north-1.elasticbeanstalk.com/deckbuilder"
-    // const url = "https://mtg-backend-bert.com/deckbuilder"
-    // const url = "https://mtg-backend-bert.elasticbeanstalk.com/deckbuilder"
+    // const url = "http://localhost:3001/deckbuilder"
+    const url = "https://api.mtg-edh-generator-backend-bert.uk/deckbuilder"
     try {
         const res = await fetch(`${url}/${decodeURIComponent(param)}`, {
             method: "GET",
